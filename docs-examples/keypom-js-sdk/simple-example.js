71d7b81b3819a921de741586b2542bf5fa9e4873
Min Qian Lu <mq2lu@uwaterloo.ca>
2023-01-24 14:35:28 -0500

    part way through updating some of the docs-example scripts

--- docs-examples/keypom-js-sdk/simple-example.js
+++ docs-examples/keypom-js-sdk/simple-example.js
@@ -1,47 +1,25 @@
 const keypom = require("../../lib");
 const { updateFunder } = require("../../lib/lib/keypom");
-const {
-	execute,
-	initKeypom,
-	getEnv,
-	createDrop,
-	getDrops,
-	claim,
-	deleteKeys,
-	deleteDrops,
-	addKeys,
-	generateKeys,
-} = keypom
-// const path = require("path");
-// const homedir = require("os").homedir();
-// const network = "testnet"
-// const myAccountId = "keypom-docs-demo.testnet"
-// const credentialsPath =  path.join(homedir, `.near-credentials/${network}/${myAccountId}.json`);
-// const myAccountInfo = require(credentialsPath)
+const { initKeypom, createDrop } = keypom
 
 async function simpleDropKeypom(){
-// If a NEAR connection is not passed in and is not already running, initKeypom will create a new connection
-// Here we are connecting to the testnet network
-await initKeypom({
-    network: "testnet", 
-    funder: {
-        accountId: "keypom-docs-demo.testnet", 
-        secretKey: "ed25519:66rWCwr7RTRpcLvpzCapSejmTBNbuWTDupv9T8H6YuYkgtcHeVKm3CgbPHTyD8VmidpfG7i3xnComzfnt4o5JfkU"
-    }
-});
+	// If a NEAR connection is not passed in and is not already running, initKeypom will create a new connection
+	// Here we are connecting to the testnet network
+	await initKeypom({
+	    network: "testnet", 
+	    funder: {
+	        accountId: "keypom-docs-demo.testnet", 
+	        secretKey: "ed25519:66rWCwr7RTRpcLvpzCapSejmTBNbuWTDupv9T8H6YuYkgtcHeVKm3CgbPHTyD8VmidpfG7i3xnComzfnt4o5JfkU"
+	    }
+	});
 
-const { fundingAccount: keypomFundingAccount } = getEnv()
-fundingAccount = keypomFundingAccount
-console.log("funding account: ", fundingAccount)
-
-
-// Note that the SDK does error checks to ensure all the information passed in will succeed when creating a drop.
-// If any information is not valid, the SDK will panic and the drop will NOT be created.
-// These checks include, but are not limited to, valid configurations, enough attached deposit, and drop existence.
-await createDrop({
-    numKeys: 1,
-    depositPerUseNEAR: "1",
-});
+	// Note that the SDK does error checks to ensure all the information passed in will succeed when creating a drop.
+	// If any information is not valid, the SDK will panic and the drop will NOT be created.
+	// These checks include, but are not limited to, valid configurations, enough attached deposit, and drop existence.
+	await createDrop({
+	    numKeys: 1,
+	    depositPerUseNEAR: "1",
+	});
 }
 
 simpleDropKeypom()
--- docs-examples/near-api-js/simple-near-example.js
+++ docs-examples/near-api-js/simple-near-example.js
@@ -5,53 +5,50 @@ const path = require("path");
 const homedir = require("os").homedir();
 
 async function simpleDropNear(){
-// Initiate connection to the NEAR blockchain.
-console.log("Initiating NEAR connection");
-const network = "testnet"
+	// Initiate connection to the NEAR blockchain.
+	const network = "testnet"
+	const CREDENTIALS_DIR = ".near-credentials";
+	const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+	
+	let keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
 
-const CREDENTIALS_DIR = ".near-credentials";
-const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+	let nearConfig = {
+		networkId: network,
+		keyStore: keyStore,
+		nodeUrl: `https://rpc.${network}.near.org`,
+		walletUrl: `https://wallet.${network}.near.org`,
+		helperUrl: `https://helper.${network}.near.org`,
+		explorerUrl: `https://explorer.${network}.near.org`,
+	};
 
-let keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+	let near = await connect(nearConfig);
+	const fundingAccount = await near.account('keypom-docs-demo.testnet');
 
-let nearConfig = {
-	networkId: network,
-	keyStore: keyStore,
-	nodeUrl: `https://rpc.${network}.near.org`,
-	walletUrl: `https://wallet.${network}.near.org`,
-	helperUrl: `https://helper.${network}.near.org`,
-	explorerUrl: `https://explorer.${network}.near.org`,
-};
+	// Keep track of an array of the key pairs we create and the public keys we pass into the contract
+	let keyPairs = [];
+	let pubKeys = [];
+	// Generate keypairs and store them into the arrays defined above
+	let keyPair = await KeyPair.fromRandom('ed25519'); 
+	keyPairs.push(keyPair);   
+	pubKeys.push(keyPair.publicKey.toString());   
 
-let near = await connect(nearConfig);
-const fundingAccount = await near.account('keypom-docs-demo.testnet');
-
-// Keep track of an array of the key pairs we create and the public keys we pass into the contract
-let keyPairs = [];
-let pubKeys = [];
-console.log("Creating keypairs");
-// Generate keypairs and store them into the arrays defined above
-let keyPair = await KeyPair.fromRandom('ed25519'); 
-keyPairs.push(keyPair);   
-pubKeys.push(keyPair.publicKey.toString());   
-
-// Create drop with pub keys, deposit_per_use
-// Note that the user is responsible for error checking when using NEAR-API-JS
-// The SDK automatically does error checking; ensuring valid configurations, enough attached deposit, drop existence etc.
-try {
-	await fundingAccount.functionCall(
-		'v1-3.keypom.testnet', 
-		'create_drop', 
-		{
-			public_keys: pubKeys,
-			deposit_per_use: parseNearAmount('1'),
-		}, 
-		"300000000000000",
-		// Attached deposit of 1 $NEAR
-		parseNearAmount("1"),
-	);
-} catch(e) {
-	console.log('error creating drop: ', e);
-}
+	// Create drop with pub keys, deposit_per_use
+	// Note that the user is responsible for error checking when using NEAR-API-JS
+	// The SDK automatically does error checking; ensuring valid configurations, enough attached deposit, drop existence etc.
+	try {
+		await fundingAccount.functionCall(
+			'v1-3.keypom.testnet', 
+			'create_drop', 
+			{
+				public_keys: pubKeys,
+				deposit_per_use: parseNearAmount('1'),
+			}, 
+			"300000000000000",
+			// Attached deposit of 1 $NEAR
+			parseNearAmount("1"),
+		);
+	} catch(e) {
+		console.log('error creating drop: ', e);
+	}
 }
 simpleDropNear()
