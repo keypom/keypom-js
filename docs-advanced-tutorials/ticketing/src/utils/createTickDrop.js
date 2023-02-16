18c89865fde8c441eb6084c4895411badcddb1b6
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-02-16 16:24:26 -0500

    auto-scan and claim tickets, more styling, tested with password protected drops

---
+++ docs-advanced-tutorials/ticketing/src/utils/createTickDrop.js
@@ -0,0 +1,254 @@
+const { initKeypom, createDrop, createNFTSeries, addToBalance } = require("keypom-js");
+const { KeyPair, keyStores, connect } = require("near-api-js");
+const { parseNearAmount } = require("near-api-js/lib/utils/format");
+const path = require("path");
+const homedir = require("os").homedir();
+
+async function createTickDrop(){
+    // Initiate connection to the NEAR blockchain.
+	const network = "testnet"
+	const CREDENTIALS_DIR = ".near-credentials";
+	const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+	
+	let keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+	let nearConfig = {
+	    networkId: network,
+	    keyStore: keyStore,
+	    nodeUrl: `https://rpc.${network}.near.org`,
+	    walletUrl: `https://wallet.${network}.near.org`,
+	    helperUrl: `https://helper.${network}.near.org`,
+	    explorerUrl: `https://explorer.${network}.near.org`,
+	};
+
+	let near = await connect(nearConfig);
+	const fundingAccount = await near.account('minqi.testnet');
+
+	// If a NEAR connection is not passed in and is not already running, initKeypom will create a new connection
+	// Here we are connecting to the testnet network
+	await initKeypom({
+	    near: near,
+	    network: "testnet"
+	});
+
+    // await addToBalance({
+    //     account: fundingAccount,
+    //     amount: "15"
+    // });
+
+    // Create drop with 30 key uses
+    let {keys, dropId} = await createDrop({
+        account: fundingAccount,
+        numKeys: 1,
+        config: {
+            usesPerKey: 30
+        },
+        metadata: "My Cool Drop Title!",
+        depositPerUseNEAR: "0.1",
+        basePassword: "event-password",
+        passwordProtectedUses: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29],
+        fcData: {
+            methods: [
+                // REPLICATE THIS 10 TIMES
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+                null,
+                [
+                    {
+                        receiverId: `nft-v2.keypom.testnet`,
+                        methodName: "nft_mint",
+                        args: "",
+                        dropIdField: "mint_id",
+                        accountIdField: "receiver_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+
+            ]
+        }
+    })
+    
+    let pubKeys = keys.publicKeys
+
+    console.log("1")
+    
+    const res = await createNFTSeries({
+        account: fundingAccount,
+        dropId,
+        metadata: {
+            title: "Moon NFT Ticket!",
+            description: "A cool NFT POAP for the best dog in the world.",
+            media: "bafybeibwhlfvlytmttpcofahkukuzh24ckcamklia3vimzd4vkgnydy7nq",
+            copies: 30
+        }
+    });
+
+    console.log("2")
+
+    var dropInfo = {};
+	const KEYPOM_CONTRACT = "v1-4.keypom.testnet"
+    // Creating list of pk's and linkdrops; copied from orignal simple-create.js
+    for(var i = 0; i < keys.keyPairs.length; i++) {
+	    let linkdropUrl = `https://testnet.mynearwallet.com/linkdrop/${KEYPOM_CONTRACT}/${keys.secretKeys[i]}`;
+	    dropInfo[pubKeys[i]] = linkdropUrl;
+	}
+
+	// Write file of all pk's and their respective linkdrops
+	console.log('Public Keys and Linkdrops: ', dropInfo)
+	console.log(`Keypom Contract Explorer Link: explorer.${network}.near.org/accounts/${KEYPOM_CONTRACT}.com`)
+}
+
+createTickDrop()
+
