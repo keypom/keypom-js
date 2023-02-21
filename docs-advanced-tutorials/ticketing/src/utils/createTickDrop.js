bc3221d1b95a88aa8bd660a4899df7667a0cfe45
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-02-21 12:59:50 -0500

    cleaned up createTickDrop.js

--- docs-advanced-tutorials/ticketing/src/utils/createTickDrop.js
+++ docs-advanced-tutorials/ticketing/src/utils/createTickDrop.js
@@ -30,7 +30,7 @@ async function createTickDrop(){
         near: near,
         network: "testnet"
     }); 
-    
+
     // Create drop with 10 keys and 2 key uses each
     let {keys, dropId} = await createDrop({
         account: fundingAccount,
@@ -41,7 +41,6 @@ async function createTickDrop(){
         metadata: "My Cool Drop Title!",
         depositPerUseNEAR: "0.1",
         basePassword: "event-password",
-        // passwordProtectedUses: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29],
         passwordProtectedUses: [1],
         fcData: {
             methods: [
