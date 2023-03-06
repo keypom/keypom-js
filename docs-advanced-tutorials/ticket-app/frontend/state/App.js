244d3a8f18491288d5038d735a057bbd208d1eea
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-03-06 12:06:54 -0500

    refactored app.js to remove useEffect

--- docs-advanced-tutorials/ticket-app/frontend/state/App.js
+++ docs-advanced-tutorials/ticket-app/frontend/state/App.js
@@ -12,55 +12,54 @@ import { initKeypom, formatLinkdropUrl } from "keypom-js";
 const { keyStores, connect } = nearAPI;
 
 
+async function connectNear(privateKey){
+  const myKeyStore = new keyStores.BrowserLocalStorageKeyStore();
+  const connectionConfig = {
+     networkId: NETWORK_ID,
+     keyStore: myKeyStore,
+     nodeUrl: `https://rpc.${NETWORK_ID}.near.org`,
+     walletUrl: `https://wallet.${NETWORK_ID}.near.org`,
+     helperUrl: `https://helper.${NETWORK_ID}.near.org`,
+     explorerUrl: `https://explorer.${NETWORK_ID}.near.org`,
+  };
+  const nearConnection = await connect(connectionConfig);
+  await initKeypom({
+      near: nearConnection,
+      network: NETWORK_ID
+  });
+  let resLink = formatLinkdropUrl({
+    customURL: "https://testnet.mynearwallet.com/linkdrop/CONTRACT_ID/SECRET_KEY",
+    secretKeys: privateKey
+  })
+  return resLink
+}
+
+async function setup(){
+  //setting contract id, priv key and link state variables.
+  const splitResultTemp = window.location.href.split("/");
+  tempContractId = splitResultTemp[3]
+  tempPrivKey = splitResultTemp[4]
+  tempLink = await connectNear(tempPrivKey)
+  console.log(tempLink)
+}
+
+let tempContractId = ""
+let tempPrivKey = ""
+let tempLink = ""
+setup()
+
+
 function App() {
-  // state variables
-  const [contractId, setContractId] = useState("")
-  const [privKey, setprivKey] = useState("")
+  //state variables
+  const [contractId, setContractId] = useState(tempContractId)
+  const [privKey, setprivKey] = useState(tempPrivKey)
   const [splitRes, setSplitRes] = useState([])
   const [pubKey, setPubkey] = useState("");
   const [curUse, setCurUse] = useState(0);
-  const [link, setLink] = useState("")
-
-
-  useEffect(() => {
-    function makeLink(privateKey){
-      var tempLink = formatLinkdropUrl({
-        customURL: "https://testnet.mynearwallet.com/linkdrop/CONTRACT_ID/SECRET_KEY",
-        secretKeys: privateKey
-      })
-      setLink(tempLink)
-    }
-
-    async function connectNear(privateKey){
-      const myKeyStore = new keyStores.BrowserLocalStorageKeyStore();
-      const connectionConfig = {
-         networkId: NETWORK_ID,
-       keyStore: myKeyStore,
-       nodeUrl: `https://rpc.${NETWORK_ID}.near.org`,
-        walletUrl: `https://wallet.${NETWORK_ID}.near.org`,
-        helperUrl: `https://helper.${NETWORK_ID}.near.org`,
-        explorerUrl: `https://explorer.${NETWORK_ID}.near.org`,
-      };
-      const nearConnection = await connect(connectionConfig);
-      await initKeypom({
-          near: nearConnection,
-          network: NETWORK_ID
-      });
-
-      makeLink(privateKey)
-    }
-    //setting contract id, priv key and link state variables.
-    const splitResultTemp = window.location.href.split("/");
-    setSplitRes(splitResultTemp)
-    setContractId(splitResultTemp[3])
-    setprivKey(splitResultTemp[4])
-    console.log("connectNear")
-    connectNear(splitResultTemp[4])
-  },[])
+  const [link, setLink] = useState(tempLink)
 
 
   // rendering stuff
-  console.log(curUse)
   if(curUse == 1){
     // use 1, should show qr code 
     console.log("scenario 1, QR code")
