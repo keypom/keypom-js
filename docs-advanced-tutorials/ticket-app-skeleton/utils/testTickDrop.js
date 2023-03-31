af1e0da58129eb34f5e3580faefa48b86fcca832
BenKurrek <bkurrek@uwaterloo.ca>
2023-03-31 16:19:39 -0400

    added console logs to testing function in skeleton

--- docs-advanced-tutorials/ticket-app-skeleton/utils/testTickDrop.js
+++ docs-advanced-tutorials/ticket-app-skeleton/utils/testTickDrop.js
@@ -9,10 +9,19 @@ async function doubleClaimCheck() {
 }
 
 async function tests() {
-    await wrongPasswordCheck();
-    await doubleClaimCheck();
-}
+    let pwResponses = await wrongPasswordCheck();
+    let dcResponses = await doubleClaimCheck();
 
+    console.log(`
+        Password Test Responses:
+        Expected false, got: ${pwResponses[0]}
+        Expected true, got: ${pwResponses[1]}
+
+        Double Claim Test Responses:
+        Expected true, got: ${dcResponses[0]}
+        Expected false, got: ${dcResponses[1]}
+    `);
+}
 
 tests()
 
