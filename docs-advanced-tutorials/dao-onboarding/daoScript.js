e2db549d2d64db20c270f7c1bd9fb2b3ab02b16e
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-04-03 15:53:08 -0400

    added fc drop to add new proposal to add member, cannot auto-approve out of the box. need to modify smart contract

---
+++ docs-advanced-tutorials/dao-onboarding/daoScript.js
@@ -0,0 +1,102 @@
+const path = require("path");
+const homedir = require("os").homedir();
+const { KeyPair, keyStores, connect, Account } = require("near-api-js");
+var assert = require('assert');
+
+const keypom = require("keypom-js");
+const {
+	initKeypom,
+	getEnv,
+	createDrop,
+    parseNearAmount,
+    createNFTSeries,
+    formatLinkdropUrl
+} = keypom
+
+// Change this to your account ID
+const FUNDER_ACCOUNT_ID = "minqi.testnet";
+const NETWORK_ID = "testnet";
+async function createDAODrop() {
+    // Initiate connection to the NEAR blockchain.
+    const CREDENTIALS_DIR = ".near-credentials";
+    const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+
+    let keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);  
+
+    let nearConfig = {
+        networkId: NETWORK_ID,
+        keyStore: keyStore,
+        nodeUrl: `https://rpc.${NETWORK_ID}.near.org`,
+        walletUrl: `https://wallet.${NETWORK_ID}.near.org`,
+        helperUrl: `https://helper.${NETWORK_ID}.near.org`,
+        explorerUrl: `https://explorer.${NETWORK_ID}.near.org`,
+    };  
+
+    let near = await connect(nearConfig);
+    const fundingAccount = new Account(near.connection, FUNDER_ACCOUNT_ID)
+    
+    // If a NEAR connection is not passed in and is not already running, initKeypom will create a new connection
+    // Here we are connecting to the testnet network
+    await initKeypom({
+        near,
+        network: NETWORK_ID,
+    });
+
+    // Create drop with 10 keys and 2 key uses each
+    let {keys, dropId} = await createDrop({
+        account: fundingAccount,
+        numKeys: 1,
+        config: {
+            usesPerKey: 1
+        },
+        depositPerUseNEAR: "0.1",
+        fcData: {
+            methods: [
+                [
+                    {
+                        receiverId: `moon-dao.sputnikv2.testnet`,
+                        methodName: "add_proposal",
+                        args: JSON.stringify(
+                            {
+                              "proposal": {
+                                "description": "Add New Onboardee",
+                                "kind": {
+                                  "AddMemberToRole": {
+                                    "role": "Onboardee"
+
+                                  }
+                                }
+                              }
+                            }
+                        ),
+                        accountIdField: "proposal.kind.AddMemberToRole.member_id",
+                        attachedDeposit: parseNearAmount("0.1")
+                    }
+                ],
+            ] 
+              
+        }   
+    })
+
+    const {contractId: KEYPOM_CONTRACT} = getEnv()
+    let tickets = formatLinkdropUrl({
+        customURL: "https://testnet.mynearwallet.com/linkdrop/CONTRACT_ID/SECRET_KEY",
+        secretKeys: keys.secretKeys,
+        contractId: KEYPOM_CONTRACT,
+    })
+    console.log(`
+    
+    Ticket Links: 
+    
+    ${tickets}
+    
+    `)
+
+    return keys
+}
+
+createDAODrop()
+
+module.exports = {
+    createDAODrop
+}
